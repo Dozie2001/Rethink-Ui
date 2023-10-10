efd067a7c79f6cce3ce0513f5015891fe2a9b527
Dozie2001 <davidchidozie402@gmail.com>
2023-10-10 13:38:00 +0100

    Created custom divs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent">
+      <nav className="sideBar">
+        <div className="sideBar-1">
+          <div className="logo">
+
+          </div>
+          <div className="home"></div>
+          <div className="toggle"></div>
+        </div>
+        <div className="sideBar-2">
+          <div className="settings"></div>
+          <div className="log-out"></div>
+        </div>
+
+      </nav>
+      <div className="main">
+        <div className='header'></div>
+        <div className="body">
+          <div className="body-header"></div>
+          <div className="body-header-sub"></div>
+          <div className="body-paragraph"></div>
+        </div>        
+      </div>
     </div>
   );
 }
